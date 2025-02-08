2b06d8e235eaf0d43ddac6049f9a574ff8ba1bcb
Petar Cevriz <pcevriz95@gmail.com>
2025-02-08 18:44:46 +0100

    Add a single passing test

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,11 +1,2 @@
-import pytest
-
-
-@pytest.mark.asyncio
-async def test_on_ready():
-    """Test if the bot logs in successfully and prints a message."""
-    pass
-
-
 def test_pass():
     assert 2 == 2
